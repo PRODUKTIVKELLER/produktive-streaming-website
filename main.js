4339caf1b85c30d7514615b3f7d71c86822e3af2
Stefan Bechert <stefan.bechert.inf@gmail.com>
2020-05-05 18:18:25 +0200

    SW-% [FEATURE] Recreate animation with typewriter effect and sound.

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
-const texts = [
-    'Folge "PRODUKTIVKELLER" auf YouTube – Alle 2 Wochen ein neues Video!',
-    'Gefällt dir unser Stream? Dann unterstütze "PRODUKTIVKELLER" auf Patreon!'
-];
-
 let produktiv = window.location.href.indexOf('live') !== -1;
 
-let stayTime = produktiv? 1000 * 60 : 4000;
+let stayTime = produktiv? 1000 * 60 : 10000;
 let hiddenTime = produktiv? 1000 * 60 * 5 : 2000;
 
-let i = 0;
-const heading = document.getElementById('heading');
+let heading = document.getElementById('heading');
 const bubbleSound = new Audio('bubble.ogg');
+const keystrokeSound = new Audio('keystroke.wav');
 
 window.addEventListener('load', fadeIn);
 
 function fadeIn() {
-    heading.innerText = texts[i];
+
+    const headingClone = heading.cloneNode(true);
+    heading.parentNode.replaceChild(headingClone, heading);
+
+    heading = headingClone;
+
     heading.classList.add("show");
 
     bubbleSound.play();
+
+    setTimeout(function() {
+        keystrokeSound.play();
+    }, 1950);
+
 
     setTimeout(fadeOut, stayTime);
 }
 
 function fadeOut() {
     heading.classList.remove("show");
-
-    i++;
-    if (i >= texts.length) {
-        i = 0;
-    }
 
     setTimeout(fadeIn, hiddenTime);
 }
